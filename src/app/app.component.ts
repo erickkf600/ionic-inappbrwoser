51b5e21ae46852e6e5edb0a1052d6e7031399a6d
erickkf600 <erickkf600@gmail.com>
2018-12-09 19:24:40 -0200

    browser

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, iab: InAppBrowser) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      iab.create('../assets/site.html','_self',{zoom:'no',location:'no'});
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
+
